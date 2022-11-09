72e5c203addc40527f5423de55dd6df9bc2d9ec8
Tobias Carlsson <TobiasTS@protonmail.com>
2022-11-09 20:09:34 +0100

    Update with a couple misc changes I made after being done
    Sadly Windows uses CRLF line endings so good luck reading the diff lmao

--- ma.py
+++ ma.py
@@ -1,18 +1,14 @@
 import re
+import operator as op
+import functools
 
-# Step 1
-# Read input program TODO: Implement REPL
-
-# Step 2
-# Parse Input
-
+### Parse Input
 
 def parse(form):
     # Match parenthesis and non-alpha-numeric tokens,
     # then treat substrings with '"' as literal strings,
     # and lastly match every other alphanumeric token.
     tokens = re.findall(r'[\(\)\+]|\"(?:[^\"\\]|\\[\s\S])*\"|\w+', form)
-    print("Tokens:", tokens)
 
     def req(index):
         result = []
@@ -35,46 +31,59 @@ def parse(form):
                         token = str(token)
                 result.append(token)
             index += 1
-            token = tokens[index]
+            token = tokens[index]  # TODO: Catch unbalanced parenthesis here
         return result, index
     return req(1)[0]
 
-# Step 3
-# Evaluate
+### Symbols table
+# TODO: Some stuff here does not work
 
-
+# fmt: off
 symbols = {
-    "+": "sum(args)",                      # Calculate the sum of the arguments
-    "-": "reduce(op.sub, args)",           # Subract every value from a list
-    "def": "def",                          # Assign a name to something
-    "fn": "def name arguments:\n body\n",  # Function definition
-    "py": "eval(args[0])",                 # Evaluates a string as python code
+    "+": "sum(args)",                           # Calculate the sum of the arguments
+    "-": "functools.reduce(op.sub, args)",      # Subract every value in a list
+    "*": "functools.reduce(op.mul, args, 1)",   # Multiply every value in a list
+    "def": "symbols[args[0]] = args[1:]",       # Assign a name to something
+    "fn": "lambda fnArgs[:] : args[1:]",        # Function definition
+    "py": "exec(args[0])",                      # Evaluates a string as python code
+    "print": "print(args[0])"                   # Prints the arguments
 }
 
+### Evaluate
 
 def evaluate(form):
-    """Compiles lisp code into Python and uses the eval() function to evaluate it"""
-    # This single function took me hours to get this terse lmao
-    # It simply walks *reverse* through the tree of s-expressions and replaces the deepest expression
-    # with the return value of the function, until it reaches the top of the tree
-    # As far as I know it is a completely unique way of evaluating lisp
-    # It's usually done with a stack, or using some recursion magic (Which I actually do too)
+    """Compiles lisp code into Python and evaluates it"""
+    # This function reversely walks through the tree of s-expressions and 
+    # replaces the deepest expression with the return value of the function, 
+    # until it reaches the top of the tree.
+
+    def parse_arguments(expression):
+        # The first symbol is the function of the s-expression
+        # The rest are the arguments
+        # if expression[0] == 'fn':
+        #     function = expression[0]
+        #     # fnArgs = expression[1]
+        #     args = expression[1:]
+        # else:
+        function = expression[0]
+        args = expression[1:]
+        # If the the arguments contain a known symbol, replace it
+        args = [symbols[i] if i in symbols else i for i in args]
+        return function, args
+
     while any(isinstance(i, list) for i in form):
         # First we build a list to keep a record of the indexes of the nested expressions
         expression = form
         expressionPath = []
         while any(isinstance(i, list) for i in expression):
             # Save the parent expression to check the index of the expression inside it
-            parent_expr = expression
-            # Saves the current root expression to expr
+            parentExpression = expression
+            # Saves the current root expression to expression
             expression = [i for i in expression if isinstance(i, list)][0]
             # Append the index of current root expression to the path record
-            expressionPath.append(parent_expr.index(expression))
+            expressionPath.append(parentExpression.index(expression))
 
-        # The first symbol is the function of the s-expression
-        function = expression[0]
-        # The rest are the arguments
-        args = expression[1:]
+        function, args = parse_arguments(expression)
 
         # Lastly we use the values we've gathered to replace the deepest expression with it's return value
         replace_nested_expression(
@@ -85,9 +94,10 @@ def evaluate(form):
         # ... And loop over again
 
     # After the form no longer has any nested expressions, we can simply return the return value of the form
-    function = form[0]
-    args = form[1:]
-    return eval(symbols[function])
+    expression = form
+    function, args = parse_arguments(expression)
+
+    return exec(symbols[function])
 
 
 def replace_nested_expression(expression, expressionPath, returnValue):
@@ -98,13 +108,22 @@ def replace_nested_expression(expression, expressionPath, returnValue):
     expression[lastIndex] = returnValue
 
 # Step 4
-# Print results and loop
+# Print results and loop (TODO: User input)
 
+### REPL
+# Examples: (print "Hello, world!")
+#           (print (+ 1 (+ 2 (+ 3 3) (+ 4 4))))
+#           (print (py "1 + 1"))
+#           (py "print('Hello from Python!')")
+#           (def x (+ 1 1))
+#           (print x)
+#           (py "print(symbols['x'])")
 
-inputs = ['(+ 1 (+ 2 (+ 3 3) (+ 4 4)))',
-          """(py "print('Hello World!')")""",
-          """(py "1 + 1")""", ]
-
-for input in inputs:
-    print("Input was: ", input)
-    print("Result was:", evaluate(parse(input)))
+def repl():
+    expression = input("> ")
+    try:
+        print(evaluate(parse(expression)))
+        repl()
+    except:
+        repl()
+repl()
